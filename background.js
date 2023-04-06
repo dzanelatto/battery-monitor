cde1cabc2ee76005607d064980d209900bd8aacb
dzanelatto <daniel.zanelatto@acesso.io>
2023-04-06 14:37:12 -0300

    refactor: :recycle: refact code spacing
    refact code spacing

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 function monitor(manager) {
-	  if (((manager.level * 100) < 10) && manager.charging == false) alert('Plug your charger, your battery is running out!');
-	 let level = (manager.level + '').split('.')[1] == undefined ? 100 : (manager.level + '').split('.')[1] < 10 ? (manager.level + '').split('.')[1] + "0" : (manager.level + '').split('.')[1];
-	 chrome.browserAction.setBadgeBackgroundColor({ color: "#990000" });
-	 chrome.browserAction.setBadgeText({text:'' + level + '%'});
+	if (((manager.level * 100) < 10) && manager.charging == false) alert('Plug your charger, your battery is running out!');
+	let level = (manager.level + '').split('.')[1] == undefined ? 100 : (manager.level + '').split('.')[1] < 10 ? (manager.level + '').split('.')[1] + "0" : (manager.level + '').split('.')[1];
+	chrome.browserAction.setBadgeBackgroundColor({ color: "#990000" });
+	chrome.browserAction.setBadgeText({text:'' + level + '%'});
 }
 
 navigator.getBattery().then((manager) => {
-   monitor(manager);
+	monitor(manager);
    
    manager.onlevelchange = () => { monitor(manager); };
 });
